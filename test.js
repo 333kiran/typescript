8861e0555e08b2589307789e6520ffcdc5a8fa78
333kiran <nagrakiran969@gmail.com>
2024-09-10 21:38:37 +0530

    adding example ts file

---
+++ test.js
@@ -0,0 +1,5 @@
+function test(num1, num2) {
+    console.log(num1 + num2);
+    return num1 + num2;
+}
+test(1, 2);
---
+++ test.ts
@@ -0,0 +1,9 @@
+
+
+function test(num1:number,num2:number){
+    console.log(num1+num2);
+    
+    return num1+num2;
+}
+
+test(1,2);
